6834d25d503d836f495273f3b98a6dfa05552dd0
Till Belz <till.belz@ut.ee>
2024-10-26 16:27:46 +0300

    added very first drag and drop test

--- src/js/calendarTask.js
+++ src/js/calendarTask.js
@@ -1,51 +1,40 @@
-window.onload = () => {
-    const participantGroup = 1;
-    const numberOfEvents = 22; // to debug generation set to 91
-
-    const basicCalendar = document.getElementById("basicCalendar");
-    const novelCalendar = document.getElementById("novelCalendar");
-    let currentCalendar;
-
-    if (participantGroup === 1) {
-        currentCalendar = novelCalendar;
-        basicCalendar.remove();
-    } else {
-        currentCalendar = basicCalendar;
-        novelCalendar.remove();
-    }
-
-    const calendarCells = currentCalendar.getElementsByTagName("td");
-    const cellsWithEvents = []
-    if (participantGroup === 1) {
-        const blockedCells = []
-
-        for(let i = 0; i < 73; i+=18) {
-            blockedCells.push(i + 0, i + 3, i + 11, i + 14, i + 17)
-        }
-        blockedCells.push(90, 93, 96, 99, 110, 113, 116, 119, 122, 125);
-
-        for(let i = 0; i < numberOfEvents; i++) {
-            let randomCell;
-            do {
-                randomCell = Math.floor(Math.random() * 126);
-            } while (blockedCells.includes(randomCell) || cellsWithEvents.includes(randomCell));
-            cellsWithEvents.push(randomCell);
-        }
-    } else {
-        for(let i = 0; i < numberOfEvents; i++) {
-            let randomCell;
-            do {
-                randomCell = Math.floor(Math.random() * 91);
-            } while (cellsWithEvents.includes(randomCell));
-            cellsWithEvents.push(randomCell);
-        }
-    }
-
-    for (const cellWithEvent of cellsWithEvents) {
-
-        calendarCells[cellWithEvent].style.setProperty("background-color", "#4477CC");
-        calendarCells[cellWithEvent].style.setProperty("color", "white");
-        calendarCells[cellWithEvent].innerHTML = calendarCells[cellWithEvent].innerHTML + ' <span style="font-weight:bold">Event</span>';
-    }
+function allowDrop(ev) {
+    ev.preventDefault();
+  }
+  
+function dragStart(ev) {
+    ev.dataTransfer.setData("eventId", ev.target.id);
+    ev.dataTransfer.setData("eventName", ev.target.textContent);
 }
+
+function drop(ev) {
+    ev.preventDefault();
+    const eventId = ev.dataTransfer.getData("eventId");
+    const eventName = ev.dataTransfer.getData("eventName");
+    const cellId = ev.dataTransfer.getData("cellId");
+    if (cellId != "") {
+        const oldCell = document.getElementById(cellId);
+        oldCell.textContent = oldCell.textContent.split(" ")[0];
+    } else {
+        document.getElementById(eventId).remove();
+    }
+
+    ev.target.textContent = ev.target.textContent + " " + eventName;
+
+    ev.target.draggable = true;
+    ev.target.dataset.eventId = eventId
+    ev.target.dataset.eventName = eventName
+    ev.target.ondragstart = (ev) => {
+        console.log("dragStartCell");
+        ev.dataTransfer.setData("eventId", ev.target.dataset.eventId);
+        ev.dataTransfer.setData("eventName", ev.target.dataset.eventName);
+        ev.dataTransfer.setData("cellId", ev.target.id);
+    };
+}
+
+const dragStartCell = (ev) => {
+    console.log("dragStartCell");
+
+}
+
 
